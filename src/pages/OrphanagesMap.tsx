c977f71bdcbfb8547b3ea6ebf684bb55e08b0ab0
Andre Rodrigues Lima <andrezaga9@hotmail.com>
2020-10-14 22:55:43 +1100

    map init

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import {FiPlus} from 'react-icons/fi'
 import mapMarkerImg from '../images/map-marker.svg'
+import {Map,TileLayer} from 'react-leaflet'
+import 'leaflet/dist/leaflet.css';
+
 import '../styles/pages/orphanages-map.css'
 function OrphanageMap(){
     return(
@@ -17,7 +20,19 @@ function OrphanageMap(){
             <span>NSW</span>
           </footer>
         </aside>
-        <div></div>
+        <Map
+        center={[-33.9032528,151.1763116]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+        
+        {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+         */}
+        <TileLayer 
+          url={`https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} 
+        />
+      
+      </Map>
         <Link to="" className="create-orphanage">
             <FiPlus size={32} color="#FFF"/>
         </Link>
